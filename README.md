12c1f6f736982710560bfc536c508aaae2df8f07
nttd-zhangxb <xibin.zhang@nttdata.com>
2020-08-06 23:32:36 +0800

    create

---
+++ README.md
@@ -0,0 +1,8 @@
+# GitHub Action - Releases API
+This GitHub Action (written in JavaScript) wraps the [GitHub Release API](https://developer.github.com/v3/repos/releases/), specifically the [Download a Release Asset](https://developer.github.com/v3/repos/releases/#get-a-release-asset) endpoint, to allow you to leverage GitHub Actions to download release assets.
+
+## Usage
+TODO...
+
+## License
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ src/download-release-asset.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core');
+const { GitHub } = require('@actions/github');
+const request = require('request');
+const fs = require('fs');
+
+async function run() {
+  try {
+    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+
+    // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    const tag = core.getInput('release_tag', { required: true });
+    const assetName = core.getInput('asset_name', { required: true });
+    const owner = process.env.GITHUB_ACTOR;
+    const repo = process.env.GITHUB_REPOSITORY.replace(`${owner}/`, "");
+
+    // find asset
+    const releaseResponse = await github.repos.getReleaseByTag({ owner, repo, tag });
+    const asset = releaseResponse.data.assets.find(e => e.name == assetName);
+
+    // download 
+    const file = fs.createWriteStream(assetName);
+    const downloadRquest = request.get(asset.url, {
+      headers: {
+        "Accept": "application/octet-stream",
+        "User-Agent": "download-release-asset",
+        "Authorization": `token ${process.env.GITHUB_TOKEN}`
+      }
+    });
+    downloadRquest.pipe(file);
+
+    const done = new Promise(function (resolve, reject) {
+      file.on('finish', () => { resolve(file.path) });
+      file.on('error', reject);
+      downloadRquest.on('error', reject);
+    });
+    const path = await done;
+
+    // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    core.setOutput('file_path', path);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
